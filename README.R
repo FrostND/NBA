277b6dbfd82369b2af75e0c7c47920f8fe2a236e
Nickolas Frost <nickfrost01@gmail.com>
2023-11-05 16:14:30 -0600

    Update README.R

--- README.R
+++ README.R
@@ -1,75 +1,38 @@
 
-#
-#  Author: Nickolas Frost PhD
-#  Contact: nickfrost01@gmail.com
-#  Created: 08/17/23 
+# Author: Nickolas Frost PhD
+# Contact: nickfrost01@gmail.com
+
+# The purpose of this project is to leverage hx 
+# NBA data to predict the outcome of NBA games. The
+# goal is to create a statistical model that is more
+# accurate than other models and more accurate than 
+# subjective picks.
+
+# Data sources: 
+# 1. play-by-play data
+# 2. player statistics
+#   - NBA player rating PER
+#   - FiveThirtyeight player rating RAPTOR
+# 3. team box scores
 
 
-##~~~~~~~~~~~~~
-##  Aims   ----
-##~~~~~~~~~~~~~
-
-#  The purpose of this R project is to predict
-#  the outcome of NBA games in the 23-24 NBA season. 
-#  Historical game data is available via the hoopR 
-#  package install.packages("hoopR") 
-
-
-##~~~~~~~~~~~~~
-##  data   ----
-##~~~~~~~~~~~~~
-
-#  Several types of data are available to analyze: 
-#  play-by-play data: load_nba_pbp(YEAR)         
-#  player statistics: load_nba_player_box(YEAR) 
-#  team box scores: load_nba_team_box(YEAR)  
-
-
-##~~~~~~~~~~~~~~~~~
-## resources   ----
-##~~~~~~~~~~~~~~~~~
-
-# FiveThirtyeight also has the 
-# Raptor metric, find it at the link below 
+# Find information on Raptor metric here:
 # https://fivethirtyeight.com/features/introducing-raptor-our-new-metric-for-the-modern-nba/
 
 # Pythagorean Expectation 
 # https://en.wikipedia.org/wiki/Pythagorean_expectation#Use_in_basketball
 
-# FiveThiryeight elaboration on how models work
-# https://fivethirtyeight.com/methodology/how-our-nba-predictions-work/
-
-# FiveThirtyeight Github with code and data
-# https://github.com/fivethirtyeight
-
-# More on Fivethirtyeight models 
-#  ~  https://projects.fivethirtyeight.com/checking-our-work/
-#  ~  https://blog.albertkuo.me/post/2022-01-21-how-good-is-fivethirtyeight-s-nba-prediction-model/
+# FiveThirtyeight elaboration on how models work
+# -https://fivethirtyeight.com/methodology/how-our-nba-predictions-work/
+# -https://projects.fivethirtyeight.com/checking-our-work/
+# -https://blog.albertkuo.me/post/2022-01-21-how-good-is-fivethirtyeight-s-nba-prediction-model/
 
 
-##~~~~~~~~~~~~~~~~~
-##  my model   ----
-##~~~~~~~~~~~~~~~~~
+# Model Evaluation Metrics  
+# - predictions better than chance (50/50) 
+# - predictions better than subjective picks 
+# - predictions better than other models 
+# - predictions better than Vegas odds 
 
-# OUTCOME: win (1) or loss (0)
-# PREDICTORS: 
-#   rebounds +
-#   assists +
-#   points +
-#   home court + 
-#   players +
-#   rest days + 
-#   injuries + 
-#   ?
-
-
-##~~~~~~~~~~~~~~~~~~~~~~~
-##  Model Accuracy   ----
-##~~~~~~~~~~~~~~~~~~~~~~~
-
-#  a) predictions better than chance (50/50) 
-#  b) predictions better than subjective picks 
-#  c) predictions better than other models 
-#  d) ?
 
 
