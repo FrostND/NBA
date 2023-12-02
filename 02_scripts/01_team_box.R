189ee145abd165fcafcfda644904a931536aebf8
Nickolas Frost <nickfrost01@gmail.com>
2023-12-02 15:43:19 -0600

    Update 01_team_box.R
    Update

--- 02_scripts/01_team_box.R
+++ 02_scripts/01_team_box.R
@@ -1,19 +1,16 @@
 
 
-# 📝 Description #### 
 # Organize team box scores into a single data frame. 
 # Seasons: 2018-19, 2019-20, 2020-21, 2021-22, 2022-23
 
-
-
-# 📦 Packages ####
+# Packages ####
 library(hoopR)     # NBA Play by Play Data
 library(tidyverse) # Load the 'Tidyverse'
 library(janitor)   # Examining and Cleaning Data
 library(todor)     # Comments and More
 
-# 📂 NBA seasons ####    
 
+# NBA seasons ####    
 box_18 <- load_nba_team_box(2018)
 box_19 <- load_nba_team_box(2019)
 box_20 <- load_nba_team_box(2020)
@@ -22,8 +19,7 @@ box_22 <- load_nba_team_box(2022)
 box_23 <- load_nba_team_box(2023)
 
 
-# need seasons in single object to 
-# apply functions across them at once
+# seasons in single object to apply functions across them at once
 nest_fun <- function(df, team_ID, season, year) {
   df %>%
     group_by({{ team_ID }}, {{ season }}, {{ year }}) %>%
@@ -59,7 +55,7 @@ rm(
 # TODO: remove all-star games, and others
 #........................................
 
-# which are the all-star games?
+# which ones are the all-star games?
 tabyl(years$team_name)
 
 rm_team <- c(
