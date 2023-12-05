01667aed7a839a831004caa9aee6f04e3108c9e6
Nickolas Frost <nickfrost01@gmail.com>
2023-12-04 20:43:19 -0600

    Update 01_box_scores.R

--- 02_scripts/01_box_scores.R
+++ 02_scripts/01_box_scores.R
@@ -3,10 +3,13 @@
 # Seasons: 2018-19, 2019-20, 2020-21, 2021-22, 2022-23
 
 # packages ####
-library(psych)      # Descriptive statistics
-library(hoopR)      # NBA Play by Play Data
-library(janitor)    # Examining and Cleaning Data
-library(tidyverse)  # Load the 'Tidyverse'
+library(psych)                # Descriptive statistics
+library(hoopR)                # NBA Play by Play Data
+library(janitor)              # Examining and Cleaning Data
+library(tidyverse)            # Load the 'Tidyverse'
+library(fivethirtyeight)      # Data Behind FiveThirtyEight
+library(fivethirtyeightdata)  # Package For FiveThirtyEight datasets
+
 
 # load box scores ####
 box <- list(
@@ -14,7 +17,7 @@ box <- list(
   player_box = load_nba_player_box(2018:2023)
 )
 
-# organize data ####
+# organize box scores ####
 map(box, .f = names)
 with(box, setdiff(team_box, player_box))
     
@@ -32,4 +35,10 @@ tabyl(box$team_box$team_name)
 # clear
 rm(rm_teams)
 
+# Raptor data ####
 
+raptor <- list(
+  raptor_by_player = raptor_by_player,
+  raptor_by_team = raptor_by_team, 
+  elo_by_player = elo_by_player,
+  )
