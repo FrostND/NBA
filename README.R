e8faa52e4fc223ecfa05b045e6564c1a02a5c4c2
Nickolas Frost <nickfrost01@gmail.com>
2023-12-04 20:25:33 -0600

    Update README.R

--- README.R
+++ README.R
@@ -1,20 +1,16 @@
 
 # Author: Nickolas Frost PhD
-# Contact: nickfrost01@gmail.com
 
-# The purpose of this project is to leverage hx 
-# NBA data to predict the outcome of NBA games. The
-# goal is to create a statistical model that is more
-# accurate than other models and more accurate than 
-# subjective picks.
+# In this demonstration we will leverage hx NBA data to predict the outcome 
+# of NBA games. Our goal will be to develop a model that is more accurate 
+# than other models and better than our subjective picks.
 
 # Data sources: 
-# 1. play-by-play data
-# 2. player statistics
-#   - NBA player rating PER
-#   - FiveThirtyeight player rating RAPTOR
-# 3. team box scores
-
+# 1. NBA play-by-play data
+# 2. NBA player statistics
+#   ~ NBA player rating PER
+#   ~ FiveThirtyEight player rating: RAPTOR
+# 3. NBA team box scores
 
 # Find information on Raptor metric here:
 # https://fivethirtyeight.com/features/introducing-raptor-our-new-metric-for-the-modern-nba/
@@ -23,16 +19,16 @@
 # https://en.wikipedia.org/wiki/Pythagorean_expectation#Use_in_basketball
 
 # FiveThirtyeight elaboration on how models work
-# -https://fivethirtyeight.com/methodology/how-our-nba-predictions-work/
-# -https://projects.fivethirtyeight.com/checking-our-work/
-# -https://blog.albertkuo.me/post/2022-01-21-how-good-is-fivethirtyeight-s-nba-prediction-model/
+# ~ https://fivethirtyeight.com/methodology/how-our-nba-predictions-work/
+# ~ https://projects.fivethirtyeight.com/checking-our-work/
+# ~ https://blog.albertkuo.me/post/2022-01-21-how-good-is-fivethirtyeight-s-nba-prediction-model/
 
 
-# Model Evaluation Metrics  
-# - predictions better than chance (50/50) 
-# - predictions better than subjective picks 
-# - predictions better than other models 
-# - predictions better than Vegas odds 
+# Preliminary Model Evaluation Metrics  
+# ~ predictions better than chance (50/50) 
+# ~ predictions better than subjective picks 
+# ~ predictions better than other models 
+# ~ predictions better than Vegas odds 
 
 
 
